51295189a04a6caeb6d31a1e0737f0460218a416
Ankur Kumar <kankurcool@gmail.com>
2023-08-22 12:27:03 +0530

    updated

--- .local/scripts/powermenu/powermenu.sh
+++ .local/scripts/powermenu/powermenu.sh
@@ -15,7 +15,7 @@ $reboot
 $shutdown" | rofi -dmenu\
                   -i\
                   -p "Power"\
-                  -config "~/.local/scripts/powermenu.rasi"\
+                  -config "~/.local/scripts/powermenu/powermenu.rasi"\
                   -font "Symbols Nerd Font 12"\
                   -width "15"\
                   -lines 5\
