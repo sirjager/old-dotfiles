104c9d643b496eea2cbfe9705d78f782e88a7281
Ankur Kumar <18713058+sirjager@users.noreply.github.com>
2024-05-24 11:53:55 +0530

    neovim configs

--- .config/nvim/lua/user/plugins/lsp/mason.lua
+++ .config/nvim/lua/user/plugins/lsp/mason.lua
@@ -114,8 +114,8 @@ if neodevok then
 end
 
 
--- NOTE: astro-ls stopped working, neovim gets crashed
--- This is temporary fix, clone the following repo somewhere and install parser
--- https://github.com/virchau13/tree-sitter-astro
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.astro.install_info.url = "~/.local/share/nvim/mason/packages/tree-sitter-astro"
+-- -- NOTE: astro-ls stopped working, neovim gets crashed
+-- -- This is temporary fix, clone the following repo somewhere and install parser
+-- -- https://github.com/virchau13/tree-sitter-astro
+-- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+-- parser_config.astro.install_info.url = "~/.local/share/nvim/mason/packages/tree-sitter-astro"
